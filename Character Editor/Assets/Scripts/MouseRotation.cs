9d771da196fe57a6cf4389817b15e84ae830eadb
manavld <mlakhwani97@gmail.com>
2021-04-24 23:34:05 +0200

    Rotate Mesh with Mouse and reset

--- Character Editor/Assets/Scripts/MouseRotation.cs
+++ Character Editor/Assets/Scripts/MouseRotation.cs
@@ -16,6 +16,11 @@ public class MouseRotation : MonoBehaviour
         float rotY = Input.GetAxis("Mouse Y") * speed;
 
         transform.Rotate(Vector3.down, rotX);
-        transform.Rotate(Vector3.right, rotY);
+        transform.Rotate(Vector3.right, -rotY);
+    }
+
+    public void onResetRotation()
+    {
+        transform.localEulerAngles = new Vector3(0, 165, 0);
     }
 }
