29f70066ecf5161ce930c5149a8d5dc113e5a70f
manavld <mlakhwani97@gmail.com>
2021-04-24 19:17:38 +0200

    MenuScene UI

---
+++ Character Editor/Assets/Scripts/Selection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Selection : MonoBehaviour
+{
+    
+    public void LoadFemaleScene()
+    {
+        SceneManager.LoadScene("Female");
+    }
+
+    public void LoadMaleScene()
+    {
+        SceneManager.LoadScene("Male");
+    }
+
+}
