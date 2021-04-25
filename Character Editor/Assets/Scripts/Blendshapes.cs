4d224129b96e75f867269059394a1b7b81a27e7a
manavld <mlakhwani97@gmail.com>
2021-04-25 20:21:30 +0200

    Controlling all male blendshapes now

--- Character Editor/Assets/Scripts/Blendshapes.cs
+++ Character Editor/Assets/Scripts/Blendshapes.cs
@@ -156,6 +156,264 @@ public class Blendshapes : MonoBehaviour
     //--------------------
 
     //UPDATE ALL SLIDERS
+
+    //Head
+    public void HeadOvalSlider(float new_value)
+    {
+        Head_Oval_Value_0 = new_value;
+    }
+    public void HeadRoundSlider(float new_value)
+    {
+        Head_Round_Value_1 = new_value;
+    }
+    public void HeadSquareSlider(float new_value)
+    {
+        Head_Square_Value_2 = new_value;
+    }
+    public void ForeheadSizeSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Forehead_Big_3 = new_value;
+            Forehead_Small_4 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Forehead_Small_4 = -new_value;
+            Forehead_Big_3 = 0;
+        }
+    }
+    public void ForeheadWidthSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Forehead_Wide_5 = new_value;
+            Forehead_Narrow_6 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Forehead_Narrow_6 = -new_value;
+            Forehead_Wide_5 = 0;
+        }
+    }
+    public void CheekbonesPositionSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Cheekbones_High_8 = new_value;
+            Cheekbones_Low_7 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Cheekbones_Low_7 = -new_value;
+            Cheekbones_High_8 = 0;
+        }
+    }
+
+
+    //Nose
+    public void NoseBaseWidthSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            NoseBase_Wide_Value_11 = new_value;
+            NoseBase_Narrow_Value_10 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            NoseBase_Narrow_Value_10 = -new_value;
+            NoseBase_Wide_Value_11 = 0;
+        }
+    }
+    public void NoseBaseLengthSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            NoseBase_Long_Value_12 = new_value;
+            NoseBase_Short_Value_13 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            NoseBase_Short_Value_13 = -new_value;
+            NoseBase_Long_Value_12 = 0;
+        }
+    }
+    public void NostrilsWidth(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            NoseNostrils_Wide_Value_14 = new_value;
+            NoseNostrils_Narrow_Value_15 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            NoseNostrils_Narrow_Value_15 = -new_value;
+            NoseNostrils_Wide_Value_14 = 0;
+        }
+    }
+    public void NosePointStraightFrontSlider(float new_value)
+    {
+        NosePoint_StraightFront_Value_16 = new_value;
+    }
+    public void NosePointRoundFrontSlider(float new_value)
+    {
+        NosePoint_RoundFront_Value_17 = new_value;
+    }
+    public void NosePointStraightProfileSlider(float new_value)
+    {
+        NosePoint_StraightProfile_Value_18 = new_value;
+    }
+    public void NosePointRoundProfileSlider(float new_value)
+    {
+        NosePoint_RoundProfile_Value_19 = new_value;
+    }
+    public void NosePointWidthSlider(float new_value)
+    {
+        NosePoint_Wide_Value_20 = new_value;
+    }
+    public void NoseBumpSlider(float new_value)
+    {
+        NoseBase_Bump_Value_21 = new_value;
+    }
+    public void NosePointPositionSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            NosePoint_High_Value_22 = new_value;
+            NosePoint_Low_Value_23 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            NosePoint_Low_Value_23 = -new_value;
+            NosePoint_High_Value_22 = 0;
+        }
+    }
+
+    //Ear
+    public void EarSizeSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Ear_Big_Value_24 = new_value;
+            Ear_Small_Value_25 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Ear_Small_Value_25 = -new_value;
+            Ear_Big_Value_24 = 0;
+        }
+    }
+    public void EarLobesSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            EarLobes_Attached_Value_27 = new_value;
+            EarLobes_Detached_Value_26 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            EarLobes_Detached_Value_26 = -new_value;
+            EarLobes_Attached_Value_27 = 0;
+        }
+    }
+    public void EarDistanceSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Ear_FarFromHead_Value_29 = new_value;
+            Ear_CloseToHead_Value_28 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Ear_CloseToHead_Value_28 = -new_value;
+            Ear_FarFromHead_Value_29 = 0;
+        }
+    }
+
+    //Chin
+    public void ChinProminenceSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            ChinProfile_Strong_Value_30 = new_value;
+            ChinProfile_Slight_Value_31 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            ChinProfile_Slight_Value_31 = -new_value;
+            ChinProfile_Strong_Value_30 = 0;
+        }
+    }
+    public void ChinPositionSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            ChinFront_High_Value_33 = new_value;
+            ChinFront_Low_Value_32 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            ChinFront_Low_Value_32 = -new_value;
+            ChinFront_High_Value_33 = 0;
+        }
+    }
+    public void ChinShapeSquareSlider(float new_value)
+    {
+        ChinShape_Square_Value_34 = new_value;
+    }
+    public void ChinShapeRoundSlider(float new_value)
+    {
+        ChinShape_Round_Value_35 = new_value;
+    }
+    public void ChinShapePointySlider(float new_value)
+    {
+        ChinShape_Pointy_Value_36 = new_value;
+    }
+
+    //Mouth
+    public void MouthWidthSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            Lips_Wide_Value_38 = new_value;
+            Lips_Narrow_Value_37 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            Lips_Narrow_Value_37 = -new_value;
+            Lips_Wide_Value_38 = 0;
+        }
+    }
+    public void UpperLipSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            LipsUpper_Thick_Value_40 = new_value;
+            LipsUpper_Thin_Value_39 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            LipsUpper_Thin_Value_39 = -new_value;
+            LipsUpper_Thick_Value_40 = 0;
+        }
+    }
+    public void LowerLipsSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            LipsLower_Thick_Value_42 = new_value;
+            LipsLower_Thin_Value_41 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            LipsLower_Thin_Value_41 = -new_value;
+            LipsLower_Thick_Value_42 = 0;
+        }
+    }
+
+
+
+    //Eyes
     public void EyesWidthSlider(float new_value)
     {
         if (new_value >= 0)
@@ -169,7 +427,29 @@ public class Blendshapes : MonoBehaviour
             Eyes_Wide_Value_46 = 0;
         }
     }
+    public void EyesSizeSlider(float new_value)
+    {
+        if (new_value >= 0)
+        {
+            EyesSize_Big_Value_48 = new_value;
+            EyesSize_Small_Value_47 = 0;
+        }
+        else if (new_value <= 0)
+        {
+            EyesSize_Small_Value_47 = -new_value;
+            EyesSize_Big_Value_48 = 0;
+        }
+    }
+    public void EyesShapeRoundSlider(float new_value)
+    {
+        EyesShape_Round_Value_49 = new_value;
+    }
+    public void EyesShapeAlmondSlider(float new_value)
+    {
+        EyesShape_Almond_Value_50 = new_value;
+    }
 
+    //Body Types
     public void EctomorphValue(float new_value)
     {
         BodyTypeEctomorph_Value_51 = new_value;
